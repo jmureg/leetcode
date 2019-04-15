2849f28d1cc5588ffe49dcebd24fc9d9a529fabd
MUELLER, JULIUS <jm169k@att.com>
2019-04-14 21:36:53 -0700

    add dynamic programming

---
+++ src/main/java/DynamicProgramming/BestTimetoBuyandSellStock.java
@@ -0,0 +1,38 @@
+package DynamicProgramming;
+
+public class BestTimetoBuyandSellStock {
+    public static void main(String[] args) {
+        int[] prices = null;
+
+        prices = new int[]{7,1,5,3,6,4};
+        System.out.println("7 = " + maxProfit(prices));
+
+        prices = new int[]{1,2,3,4,5};
+        System.out.println("4 = " + maxProfit(prices));
+
+        prices = new int[]{1};
+        System.out.println("0 = " + maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        if(prices.length==0) {
+            return 0;
+        }
+
+        int min = 0;
+        int max = 0;
+        int cur = 0;
+
+        for(int i=1; i<prices.length; i++) {
+            if(prices[i]<prices[min]) {
+                min = i;
+            } else {
+                cur = prices[i]-prices[min];
+                if(cur>max) {
+                    max = cur;
+                }
+            }
+        }
+        return max;
+    }
+}
