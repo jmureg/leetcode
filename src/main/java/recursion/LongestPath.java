5c98a32eb680845baf23cb7d960306c180559181
MUELLER, JULIUS <jm169k@att.com>
2019-04-25 11:22:55 -0700

    update example with optimal solution

--- src/main/java/recursion/LongestPath.java
+++ src/main/java/recursion/LongestPath.java
@@ -1,12 +1,17 @@
-package arrays;
+package recursion;
 
-// 2d array of 1s and 0s
-// we want to find the longest contiguous path in the array of 1s
-// 0s = water
-// 1s = land
-// find the biggest island!
+/*
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
 
+Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
 
+ 2d array of 1s and 0s
+ we want to find the longest contiguous path in the array of 1s
+ 0s = water
+ 1s = land
+ find the biggest island!
+ https://leetcode.com/problems/max-area-of-island/
+ */
 public class LongestPath {
     public static void main(String args[] ) throws Exception {
 
@@ -58,15 +63,61 @@ public class LongestPath {
                 new int[]{0, 0, 1, 0}
         };
         System.out.println("expected 6: " + lp.longestPath(ex4));
+
+        int[][] ex5 = new int[][]{
+                new int[]{0}
+        };
+        System.out.println("expected 0: " + lp.longestPath(ex5));
+
+        int[][] ex6 = new int[][]{
+                new int[]{0, 0, 0},
+                new int[]{0, 1, 0},
+                new int[]{0, 0, 0}
+        };
+        System.out.println("expected 1: " + lp.longestPath(ex6));
+
+        int[][] ex7 = new int[][]{
+                new int[]{1,1}
+        };
+        System.out.println("expected 1: " + lp.longestPath(ex6));
     }
 
-    public int longestPath(int[][] matrix) {
+/*
+source https://leetcode.com/problems/max-area-of-island/solution/
+*/
+
+    int[][] grid;
+    boolean[][] seen;
+
+    public int longestPath(int[][] grid) {
+        this.grid = grid;
+        seen = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r, c));
+            }
+        }
+        return ans;
+    }
+    public int area(int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
+                seen[r][c] || grid[r][c] == 0)
+            return 0;
+        seen[r][c] = true;
+        return (1 + area(r+1, c) + area(r-1, c)
+                + area(r, c-1) + area(r, c+1));
+    }
+
+
+
+    public int longestPath2(int[][] matrix) {
         int maxLength = 0;
         if(matrix==null || matrix.length==0) {
             return 0;
         }
         if(matrix.length==1 && matrix[0].length==1) {
-            return 1;
+            return (matrix[0][0]==1)? 1:0;
         }
 
         boolean[][] path = new boolean[matrix.length][matrix[0].length];
