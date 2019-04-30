13b2d93ca527db15db1b9c5e739b765fc7523701
MUELLER, JULIUS <jm169k@att.com>
2019-04-30 16:48:51 -0700

    add stub for GuessGame

---
+++ src/main/java/games/GuessGame.java
@@ -0,0 +1,23 @@
+package games;
+
+/*
+https://leetcode.com/problems/guess-number-higher-or-lower-ii/
+
+ */
+public class GuessGame {
+    GuessGame(){
+    }
+
+    public int getMoneyAmount(int n) {
+        return 0;
+    }
+
+    public static void main (String[] args) {
+        GuessGame gg = new GuessGame();
+        int k = 0;
+
+        k=8;
+        System.out.println("number selected is: " + gg.getMoneyAmount(k));
+    }
+
+}
