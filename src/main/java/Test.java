8d8a42d0a138c71b2bd30ae55d9add58e219064f
MUELLER, JULIUS <jm169k@att.com>
2019-04-30 12:17:02 -0700

    add simple tests

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,21 +6,24 @@ public class Test {
         l.add(1);
         l.size();
 
-
         Stack<String> h = new Stack<String>();
 
+        Hashtable<Integer, String> hashtable = new Hashtable<>();
+        hashtable.put(1,  "A");
+        hashtable.put(2,  "B" );
+        hashtable.put(3,  "C");
+        hashtable.put(2,  "BB" );
+        String test = hashtable.get(2);
+        System.out.println(hashtable);
 
         HashMap<Integer, String> hmap1 = new HashMap<Integer, String>();
         hmap1.put(1, "Auto");
         hmap1.containsKey("");
 
-
         HashMap<String, Integer> hmap2 = new HashMap<String, Integer>();
         hmap2.put("Auto", 1);
 
-
         LinkedList<String> linkedlist = new LinkedList<String>();
         linkedlist.size();
-
     }
 }
